9585583e3af4dc638244bd9f6c9d31a0eb43db0d
Abab-bk <55276676+Abab-bk@users.noreply.github.com>
2024-10-28 20:18:16 +0800

    Main menu

---
+++ Rainstorm/Rainstorm/Core/Scene.cs
@@ -0,0 +1,9 @@
+﻿namespace Rainstorm.Core;
+
+public abstract class Scene
+{
+    public virtual void Update() { }
+    public virtual void Draw() { }
+    public virtual void OnEnter() { }
+    public virtual void OnExit() { }
+}
